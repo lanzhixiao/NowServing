6ce8e6572fda420544f0f66e9049ca9fbc6e0f70
Lanzhi Xiao <f5u1b@ugrad.cs.ubc.ca>
2018-09-06 17:52:52 -0700

    Added class-level comment to CustomerCounterTest

--- src/test/ca/ubc/cpsc210/nowserving/test/CustomerCounterTest.java
+++ src/test/ca/ubc/cpsc210/nowserving/test/CustomerCounterTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+// Unit tests for the CustomerCounter class
 class CustomerCounterTest {
     private CustomerCounter testCounter;
     
